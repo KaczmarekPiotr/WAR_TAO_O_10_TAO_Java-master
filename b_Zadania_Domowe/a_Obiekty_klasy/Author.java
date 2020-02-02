9c67f820989a6ea93490eb1a0cc85274afc908fa
Piotr Kaczmarek <kaczmarek_piotr@o2.pl>
2020-02-02 09:21:20 +0100

    zrobione

---
+++ b_Zadania_Domowe/a_Obiekty_klasy/Author.java
@@ -0,0 +1,20 @@
+package b_Zadania_Domowe.a_Obiekty_klasy;
+
+
+public class Author {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String pseudonym;
+
+    public Author(int id, String firstName, String lastName, String pseudonym) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.pseudonym = pseudonym;
+
+
+    }
+
+}
